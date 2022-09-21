f832f63c84fa1fdb8027dc6bc1b6ea049c3d3f05
Roar <you@example.comroar.jacinto07@gmail.com>
2022-09-21 09:39:13 +0800

    first commit

---
+++ src/shared/_mock/companies.js
@@ -0,0 +1,34 @@
+const companies = [
+  {
+    id: 1,
+    name: 'KSB',
+    description: 'For pumps, valves and service. Drawing on our wide-ranging application know-how, extensive experience and close-knit service network',
+    vip: 'Content House, Inc.',
+  },
+  {
+    id: 2,
+    name: 'DELTA',
+    description: 'Domestic and international travel, offers airline tickets & flights to over 300 destinations in 60 countries',
+    vip: 'Glen Hauenstein',
+  },
+  {
+    id: 3,
+    name: 'ProSoft Technology Inc',
+    description: 'provides and developes connectivity solutions that link dissimilar automation products compatible with the large automation',
+    vip: 'Dror Rom',
+  },
+  {
+    id: 4,
+    name: 'Von Leonardo',
+    description: 'Content House, Inc.',
+    vip: 'Software Engineer',
+  },
+  {
+    id: 5,
+    name: 'Zen Batan',
+    description: 'Content House, Inc.',
+    vip: 'Software Engineer',
+  },
+]
+
+export default companies
